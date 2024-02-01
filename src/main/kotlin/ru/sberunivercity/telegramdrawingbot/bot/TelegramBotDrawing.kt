a13aa66df2fbb964b4e68f9eb9754d968e4ff8cb
Ivan Muratov <strateg8888@gmail.com>
2024-02-01 12:30:31 +0400

    Fix

--- src/main/kotlin/ru/sberunivercity/telegramdrawingbot/bot/TelegramBotDrawing.kt
+++ src/main/kotlin/ru/sberunivercity/telegramdrawingbot/bot/TelegramBotDrawing.kt
@@ -1,23 +1,32 @@
 package ru.sberunivercity.telegramdrawingbot.bot
 
+import jakarta.annotation.PostConstruct
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Component
 import org.springframework.stereotype.Service
 import org.telegram.telegrambots.bots.TelegramLongPollingBot
+import org.telegram.telegrambots.meta.TelegramBotsApi
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage
 import org.telegram.telegrambots.meta.api.objects.Update
-import java.time.ZoneId
-
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession
 
 @Service
 class TelegramBotDrawing: TelegramLongPollingBot() {
 
     @Value("\${telegram.botName}")
-    private val botName: String = ""
+    private lateinit var botName: String
 
     @Value("\${telegram.token}")
-    private val token: String = ""
+    private lateinit var token: String
     override fun getBotUsername(): String = botName
     override fun getBotToken(): String = token
+
+
+    @PostConstruct
+    fun initBot() {
+        val botApi = TelegramBotsApi(DefaultBotSession::class.java)
+        botApi.registerBot(this)
+    }
 
     override fun onUpdateReceived(update: Update) {
         if (update.hasMessage()){
