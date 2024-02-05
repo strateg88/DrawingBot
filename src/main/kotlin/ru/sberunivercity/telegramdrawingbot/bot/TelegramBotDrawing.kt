2c22bb67f22e76805a063d9518e9e0274e7aa776
Ivan Muratov <strateg8888@gmail.com>
2024-02-05 16:35:36 +0400

    Пользователь уже зарегестрирован

--- src/main/kotlin/ru/sberunivercity/telegramdrawingbot/bot/TelegramBotDrawing.kt
+++ src/main/kotlin/ru/sberunivercity/telegramdrawingbot/bot/TelegramBotDrawing.kt
@@ -4,6 +4,8 @@ import org.telegram.telegrambots.bots.TelegramLongPollingBot
 import org.telegram.telegrambots.meta.TelegramBotsApi
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage
 import org.telegram.telegrambots.meta.api.objects.Update
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession
 import java.sql.DriverManager
@@ -25,6 +27,7 @@ class TelegramBotDrawing : TelegramLongPollingBot() {
             val message = update.message
             val chatId = message.chatId
             val text = message.text
+            println("Пользователь $chatId написал: $text")
             if (text.startsWith("/")) {
                 if (text == "/start") {
                     sendMessage(
@@ -36,9 +39,32 @@ class TelegramBotDrawing : TelegramLongPollingBot() {
                 // Мой id = 245599936L - я админ
                 if (245599936L == chatId) {
                     //тут отправлять админскую клавиатуру
+                    val markupInline = InlineKeyboardMarkup()
+                    // добавляем встроенную клавиатуру в сообщение
+                    val rowsInline: MutableList<List<InlineKeyboardButton>> = ArrayList()
+                    val rowInline1: MutableList<InlineKeyboardButton> = ArrayList()
+
+                    val inlineKeyboardButton1 = InlineKeyboardButton()
+                    inlineKeyboardButton1.text = "Розыграть!";
+                    inlineKeyboardButton1.callbackData = "CB Розыграть!";
+                    rowInline1.add(inlineKeyboardButton1);
+                    rowsInline.add(rowInline1)
+
+                    markupInline.keyboard = rowsInline
+                    message.replyMarkup = markupInline
+                    sendMessage(chatId, markupInline)
+                } else {
+                    try {
+                        addToDatabase(chatId, text)
+                    } catch (exc: org.postgresql.util.PSQLException) {
+                        println(exc.message)
+                        if (exc.message != null && exc.message!!.contains("ОШИБКА: повторяющееся значение ключа нарушает ограничение уникальности", true)) {
+                            sendMessage(chatId, "Вы уже зарегистрированы!")
+                            return
+                        }
+                    }
+                    sendMessage(chatId, "Вы успешно зарегистрированы для участия в розыгрыше!")
                 }
-                addToDatabase(chatId, text)
-                sendMessage(chatId, "Вы успешно зарегистрированы для участия в розыгрыше!")
             }
         }
     }
@@ -48,6 +74,18 @@ class TelegramBotDrawing : TelegramLongPollingBot() {
         message.chatId = chatId.toString()
         message.text = text
         try {
+            execute(message)
+        } catch (e: TelegramApiException) {
+            e.printStackTrace()
+        }
+    }
+
+    fun sendMessage(chatId: Long, keyboard: InlineKeyboardMarkup) {
+        try {
+            val message = SendMessage()
+            message.chatId = chatId.toString()
+            message.replyMarkup = keyboard
+            message.text = "Клавиатура"
             execute(message)
         } catch (e: TelegramApiException) {
             e.printStackTrace()
